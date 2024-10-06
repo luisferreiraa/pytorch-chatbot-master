f3933b85fdab164019532b623a17162aecf98a6a
luisferreiraa <luis.c.ferreira.prt_a@msft.cesae.pt>
2024-10-06 21:05:22 +0100

    06102024

---
+++ chat.py
@@ -0,0 +1,67 @@
+import os
+
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import random
+import json
+import torch
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+app = Flask(__name__)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r', encoding='utf-8') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/cv')
+def get_cv():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'cv_luis.pdf')
+
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    user_input = request.json['question']
+    sentence = tokenize(user_input)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                response = random.choice(intent['responses'])
+                return jsonify({'answer': response})
+    else:
+        return jsonify({'answer': "Desculpe, não entendi."})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
